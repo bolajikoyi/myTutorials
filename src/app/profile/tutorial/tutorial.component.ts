f7739b112536e0d6f0b86928cc22fb2d0bc85030
Omobolaji Koyi <o.koyi@3dqr.de>
2021-01-10 11:22:03 +0100

    tutorial tabs clean-up

--- src/app/profile/tutorial/tutorial.component.ts
+++ src/app/profile/tutorial/tutorial.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+interface tutorialLink {
+  link: string;
+  name: string;
+}
 @Component({
   selector: 'app-tutorial',
   templateUrl: './tutorial.component.html',
-  styleUrls: ['./tutorial.component.scss']
+  styleUrls: ['./tutorial.component.scss'],
 })
 export class TutorialComponent implements OnInit {
+  tutorialLinks: tutorialLink[] = [
+    { link: 'first', name: 'Tutorial One' },
+    { link: 'second', name: 'Tutorial Two' },
+    { link: 'third', name: 'Tutorial Three' },
+    { link: 'fourth', name: 'Tutorial Four' },
+    { link: 'fifth', name: 'Tutorial Five' },
+    { link: 'sixth', name: 'Tutorial Six' },
+    { link: 'seventh', name: 'Tutorial Seven' },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
